10fd21197f8f0f3b9865327e01c2f5c77b29aa32
h181192 <h181192@stud.hib.no>
2017-05-25 17:53:35 +0200

    JavaScript versjon

---
+++ Terning_To_Spiller.js
@@ -0,0 +1,81 @@
+let har_trillt = false
+let terning = 0
+input.onGesture(Gesture.Shake, () => {
+    terning = Math.random(6) + 1
+    if (terning == 6) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    } else if (terning == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (terning == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (terning == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (terning == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.pause(1000)
+    har_trillt = true
+    radio.sendNumber(terning)
+})
+radio.onDataPacketReceived(({receivedNumber}) => {
+    while (!(har_trillt)) {
+        basic.pause(50)
+    }
+    if (terning > receivedNumber) {
+        basic.showIcon(IconNames.Heart)
+        music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    } else if (terning == receivedNumber) {
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    har_trillt = false
+    basic.clearScreen()
+})
+radio.setGroup(1)
+har_trillt = false
